337eaa6f5b50cad5439af304c44a91cbfd4c9f1e
Guilherme Stoll Toaldo <guilherme.toaldo@olaisaac.com.br>
2022-12-21 19:02:26 -0300

    d21 p2

--- d21/solve.go
+++ d21/solve.go
@@ -29,13 +29,79 @@ func runJob(node nodeT) int {
 	}
 }
 
+func containsHuman(node nodeT) bool {
+	if node.name == "humn" {
+		return true
+	}
+
+	isOnTheLeft := false
+	if node.left != nil {
+		isOnTheLeft = containsHuman(*node.left)
+	}
+
+	isOnTheRight := false
+	if node.right != nil {
+		isOnTheRight = containsHuman(*node.right)
+	}
+
+	return isOnTheLeft || isOnTheRight
+}
+
+func calcHumanValue(node nodeT) int {
+	var knownValue int
+	var otherValue int
+	var nextNode *nodeT
+	onTheLeft := false
+
+	if node.name == "humn" {
+		return node.value
+	}
+
+	if containsHuman(*node.left) {
+		knownValue = runJob(*node.right)
+		nextNode = node.left
+		onTheLeft = true
+	} else {
+		knownValue = runJob(*node.left)
+		nextNode = node.right
+	}
+
+	switch node.operator {
+	case "+":
+		if node.name == "root" {
+			otherValue = knownValue
+		} else {
+			otherValue = node.value - knownValue
+		}
+	case "-":
+		if onTheLeft {
+			otherValue = node.value + knownValue
+		} else {
+			otherValue = knownValue - node.value
+		}
+	case "*":
+		otherValue = node.value / knownValue
+	case "/":
+		if onTheLeft {
+			otherValue = node.value * knownValue
+		} else {
+			otherValue = knownValue / node.value
+		}
+	default:
+		panic("operator not found")
+	}
+
+	nextNode.value = otherValue
+	return calcHumanValue(*nextNode)
+}
+
 func part1(input inputT) {
 	answer := runJob(nodeT(input))
 	fmt.Println("part 1:", answer)
 }
 
 func part2(input inputT) {
-	answer := ""
+	answer := calcHumanValue(nodeT(input))
 	fmt.Println("part 2:", answer)
 }
 
